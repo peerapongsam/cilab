0070dd41c71239bb37d5dcc783cb8c59d66a381c
Peerapong Samarnpong <peerapong_sam@hotmail.com>
2016-05-06 13:07:54 +0700

    update register

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -3,18 +3,41 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Register extends CI_Controller {
 
-  public function __construct() {
-    parent::__construct();
-    $this->load->library(array('parser'));
-    $this->load->helper(array('form','url'));
-  }
+	public function __construct() {
+		parent::__construct();
+		$this->load->database();
+		$this->load->library(array('form_validation'));
+		$this->load->model('user_model');
+	}
 
-  public function index()
-  {
-    $data = array(
-      'title'=>'Register'
-    );
+	public function index() {
+		$data = array();
 
-    $this->parser->parse('member/register_view', $data);
-  }
+		$this->form_validation->set_rules('email', 'Email', 'required|valid_email|callback_email_check');
+		$this->form_validation->set_rules('username', 'Username', 'trim|required|alpha_dash|is_unique[users.username]');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[4]|max_length[20]');
+		$this->form_validation->set_rules('passconf', 'Password Confirmation', 'trim|required|matches[password]');
+
+		if ($this->form_validation->run() === false) {
+			$data['validation_errors'] = validation_errors();
+			$this->load->view('member/register_view', $data);
+		} else {
+			if ($this->user_model->insert_user()) {
+				$this->load->view('member/register_success_view');
+			} else {
+				$data['validation_errors'] = 'Unable create account';
+				$this->load->view('member/register_view', $data);
+			}
+		}
+
+	}
+
+	public function email_check($email) {
+		if ($this->user_model->get_user_by_email($email) != null) {
+			$this->form_validation->set_message('email_check', 'The {field} field must contain a unique value.');
+			return false;
+		} else {
+			return true;
+		}
+	}
 }
---
+++ application/views/member/register_success_view.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Register Successfull</title>
+    <link href="<?php echo site_url("/assets/css/main.css");?>" rel="stylesheet">
+  </head>
+  <body>
+    <div class="form">
+        <h1>Register Successfull</h1>
+        <div class="form-link"><a href="<?=site_url("login");?>">Go to Login Page</a></div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  </body>
+</html>
